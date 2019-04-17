5ef8caad0fbeab8c30cef13ddb513b2bb11f391e
Adrian Samaniego <llathander@gmail.com>
2019-04-17 00:10:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,16 +2,17 @@
 
 def countdown(x)
   while x > 0
-  puts "#{x} SECOND(S)!"
-x -= 1
+   puts "#{x} SECOND(S)!"
+    x -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(x)
   while x > 0
-  puts "#{x} SECOND(S)!"
-x -= 1
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
